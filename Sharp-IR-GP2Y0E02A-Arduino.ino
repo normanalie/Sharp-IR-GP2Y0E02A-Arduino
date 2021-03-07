1eee12efacf83f908e9e55886567593f3fa8ea3d
Norman Alié <mail@normanalie.fr>
2021-03-07 12:08:26 +0400

    Add ino

---
+++ Sharp-IR-GP2Y0E02A-Arduino.ino
@@ -0,0 +1,22 @@
+//!!! Le capteur fonctione en 3.3v !!!
+int analogPin = A0;
+int switchModePin = 0;
+int reading;
+float distance; //−0.1441x+68.3213
+
+
+void setup(){
+    Serial.begin(9600);
+    pinMode(switchModePin, OUTPUT);
+    Serial.println("Initialized");
+    Serial.println("Wait...");
+    delay(50); //Delai de démarrage du capteur avant de le mettre en mode mesure
+    digitalWrite(0, HIGH); //Capteur en mode mesure
+}
+
+void  loop(){
+    reading = analogRead(analogPin);
+    distance = -0.1441 * reading +68.3213;
+    Serial.println(distance);
+    delay(10); //Limit number of value
+}
